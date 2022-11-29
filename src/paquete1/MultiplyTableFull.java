8dcf8991db1f7194a84619c208b9a3b8965404ae
Adrián Luján <adri.alm13@gmail.com>
2022-11-29 12:51:17 +0100

    added funcionality: ask n to user

--- src/paquete1/MultiplyTableFull.java
+++ src/paquete1/MultiplyTableFull.java
@@ -8,6 +8,7 @@ public class MultiplyTableFull {
     public class MultiplyTableFull {
         public static void main(String[] args) {
             Scanner in = new Scanner(System.in).useLocale(Locale.US);
+            int n=in.nextInt();
 
             for (int i = 1; i <= 10; i++) {
                 for (int j = 5; j <= 20; j++) {
